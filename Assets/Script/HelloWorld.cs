f36e58ee526515e75d3945021673c635ac501fca
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2022-03-23 23:46:52 +0700

    Add Scene-MainMenu

--- Assets/Script/HelloWorld.cs
+++ Assets/Script/HelloWorld.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelloWorld : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        gameObject.GetComponent<Button>().onClick.AddListener(tampil);
     }
 
     // Update is called once per frame
-    void Update()
+    private void tampil()
     {
-        
+        Debug.Log("saya taufiq");
     }
+
 }
