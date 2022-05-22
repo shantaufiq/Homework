af49de14bb201f6b6ff469f2b9b41dd624281c91
shantaufiq <shantaufiq@student.telkomuniversity.ac.id>
2022-05-22 09:10:43 +0700

    Create-Cemo Character Controler

---
+++ Assets/Script/Lobby/FPSController.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    // refrences
+    public Transform cameraTransform;
+    public CharacterController characterController;
+
+    // player setting
+    public float cameraSensitivity;
+    public float moveSpeed;
+    public float moveInputDeadZone;
+
+    // touch detection 
+    int leftFinggerID, rightFinggerID;
+    float halfScreenWidht;
+
+    // camera control
+    Vector2 lookInput;
+    float cameraPitch;
+
+    // player movement 
+    Vector2 moveTouchStartPosition;
+    Vector2 moveInput;
+
+    private void Start()
+    {
+        leftFinggerID = -1;
+        rightFinggerID = -1;
+
+        halfScreenWidht = Screen.width / 2;
+
+        // calcualte the movement input dead zone
+        moveInputDeadZone = Mathf.Pow(Screen.width / moveInputDeadZone, 2);
+    }
+
+    private void Update()
+    {
+        // handles input
+        GetTouchInput();
+
+        // check righ fingger touch
+        if (rightFinggerID != -1)
+        {
+            lookAround();
+        }
+
+        if (leftFinggerID != -1)
+        {
+            Debug.Log("moving");
+            Move();
+        }
+    }
+
+    void GetTouchInput()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch t = Input.GetTouch(i);
+
+            switch (t.phase)
+            {
+                case TouchPhase.Began:
+                    if (t.position.x < halfScreenWidht && leftFinggerID == -1)
+                    {
+                        leftFinggerID = t.fingerId;
+                        // Debug.Log("tracking left fingger");
+                        moveTouchStartPosition = t.position;
+                    }
+                    else if (t.position.x > halfScreenWidht && rightFinggerID == -1)
+                    {
+                        rightFinggerID = t.fingerId;
+                        // Debug.Log("tracking right fingger");
+                    }
+                    break;
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    if (t.fingerId == leftFinggerID)
+                    {
+                        leftFinggerID = -1;
+                        Debug.Log("stop tracking left fingger");
+                    }
+                    if (t.fingerId == rightFinggerID)
+                    {
+                        rightFinggerID = -1;
+                        Debug.Log("stop tracking right fingger");
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    // get input for looking around
+                    if (t.fingerId == rightFinggerID)
+                    {
+                        lookInput = t.deltaPosition * cameraSensitivity * Time.deltaTime;
+                    }
+                    else if (t.fingerId == leftFinggerID)
+                    {
+                        moveInput = t.position - moveTouchStartPosition;
+                    }
+                    break;
+                case TouchPhase.Stationary:
+                    // set the look input to zero if the fingger is still
+                    if (t.fingerId == rightFinggerID)
+                    {
+                        lookInput = Vector2.zero;
+                    }
+                    break;
+            }
+        }
+    }
+
+    void lookAround()
+    {
+        // vertical (pitch) rotation
+        cameraPitch = Mathf.Clamp(cameraPitch - lookInput.y, -90f, 90f);
+
+        // horizontal (yaw) rotation
+        transform.Rotate(transform.up, lookInput.x);
+    }
+
+    void Move()
+    {
+        // don't move if the touch delta is shooter than the designeted ded zone
+        if (moveInput.sqrMagnitude <= moveInputDeadZone) return;
+
+        // multiply the normalized direction by the speed
+        Vector2 movementDirection = moveInput.normalized * moveSpeed * Time.deltaTime;
+        // move relatively to the local transform direction 
+        characterController.Move(transform.right * movementDirection.x + transform.forward * movementDirection.y);
+    }
+}
